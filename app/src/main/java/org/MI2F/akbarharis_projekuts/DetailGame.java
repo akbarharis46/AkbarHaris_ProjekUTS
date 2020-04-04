116a6631847cd8c7e7f4c23d9260282a016e4b65
akbarharisn <akbarharisn792@gmail.com>
2020-04-05 00:07:59 +0700

    Progres Game Pedia 2

--- app/src/main/java/org/MI2F/akbarharis_projekuts/DetailGame.java
+++ app/src/main/java/org/MI2F/akbarharis_projekuts/DetailGame.java
@@ -3,12 +3,46 @@ package org.MI2F.akbarharis_projekuts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailGame extends AppCompatActivity {
+
+    public static final String ITEM_EXTRA = "item_extra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_game);
+
+        ImageView imgItemPhoto = findViewById(R.id.img_item_photo);
+        TextView tvName = findViewById(R.id.tv_item_name);
+        TextView tvDeskripsi = findViewById(R.id.tv_item_deskripsi);
+        TextView tvHarga = findViewById(R.id.tv_item_harga);
+
+        Game game = getIntent().getParcelableExtra(ITEM_EXTRA);
+
+        if (game != null) {
+            Glide.with(this)
+                    .load(game.getPhoto())
+                    .into(imgItemPhoto);
+            tvName.setText(game.getNama_game());
+            tvDeskripsi.setText(game.getDetail_game());
+            tvHarga.setText(game.getHarga());
+        }
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Detail Makanan");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 }
